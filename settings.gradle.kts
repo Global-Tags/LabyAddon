3f54c547f30b207dc49f09582b00d3d276f5e326
RappyTV <rappytv@rappytv.com>
2023-11-23 12:41:22 +0100

    Bump labygradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "GlobalTags"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.28"
+    val labyGradlePluginVersion = "0.3.31"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
