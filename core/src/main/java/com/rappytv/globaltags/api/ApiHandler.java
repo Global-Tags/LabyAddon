cfabe0b71240252a7c9c84344a8003198b09d6c2
RappyTV <rappytv@rappytv.com>
2023-11-24 23:24:24 +0100

    Add enum to have multiple constructors with one string

--- core/src/main/java/com/rappytv/globaltags/api/ApiHandler.java
+++ core/src/main/java/com/rappytv/globaltags/api/ApiHandler.java
@@ -1,5 +1,6 @@
 package com.rappytv.globaltags.api;
 
+import com.rappytv.globaltags.api.RequestBody.StringType;
 import com.rappytv.globaltags.api.requests.PositionSetRequest;
 import com.rappytv.globaltags.api.requests.TagSetRequest;
 import com.rappytv.globaltags.util.Util;
@@ -68,7 +69,7 @@ public class ApiHandler {
         ) {
             @Override
             public RequestBody getBody() {
-                return null;
+                return new RequestBody(reason, StringType.REPORT_REASON);
             }
         };
         request.sendAsyncRequest().thenRun(() -> {
