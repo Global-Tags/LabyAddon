33cc709d513d2b780eb69ffa59c2f31831267089
RappyTV <rappytv@rappytv.com>
2024-01-24 17:10:57 +0100

    Add resourceLocation to GlobalIcon

--- core/src/main/java/com/rappytv/globaltags/util/GlobalIcon.java
+++ core/src/main/java/com/rappytv/globaltags/util/GlobalIcon.java
@@ -1,8 +1,23 @@
 package com.rappytv.globaltags.util;
 
+import net.labymod.api.client.resources.ResourceLocation;
+
 public enum GlobalIcon {
     NONE,
     DISCORD,
     TWITCH,
-    YOUTUBE
+    YOUTUBE;
+
+    private final ResourceLocation resourceLocation;
+
+    GlobalIcon() {
+        this.resourceLocation = ResourceLocation.create(
+            "globaltags",
+            "textures/icons/" + this.name().toLowerCase() + ".png"
+        );
+    }
+
+    public ResourceLocation resourceLocation() {
+        return resourceLocation;
+    }
 }
