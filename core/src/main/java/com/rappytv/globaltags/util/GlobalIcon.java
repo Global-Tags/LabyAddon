923648352b2ecc2288b3ff3064d5ed48c7ee444f
RappyTV <rappytv@rappytv.com>
2024-01-24 23:24:00 +0100

    Add some icons bereal, ebio, epicgames, github, gitlab instagram, kick, pinterest, playstation snapchat, steam, threads, tiktok, x, xbox

--- core/src/main/java/com/rappytv/globaltags/util/GlobalIcon.java
+++ core/src/main/java/com/rappytv/globaltags/util/GlobalIcon.java
@@ -4,8 +4,22 @@ import net.labymod.api.client.resources.ResourceLocation;
 
 public enum GlobalIcon {
     NONE,
+    BEREAL,
     DISCORD,
+    EBIO,
+    EPICGAMES,
+    GITHUB,
+    GITLAB,
+    INSTAGRAM,
+    KICK,
+    PLAYSTATION,
+    SNAPCHAT,
+    STEAM,
+    THREADS,
+    TIKTOK,
     TWITCH,
+    X,
+    XBOX,
     YOUTUBE;
 
     private final ResourceLocation resourceLocation;
