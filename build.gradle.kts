740d7417caef2d0dd91ac5ca9791460e39f3e3bc
RappyTV <rappytv@rappytv.com>
2024-02-09 17:56:20 +0100

    Bump versions (5x because of 5 improvements in this pr)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ labyMod {
         author = "RappyTV"
         description = "Get yourself a custom Globaltag that's publicly visible to anyone using this addon."
         minecraftVersion = "*"
-        version = System.getenv().getOrDefault("VERSION", "1.1.0")
+        version = System.getenv().getOrDefault("VERSION", "1.1.5")
     }
 
     minecraft {
