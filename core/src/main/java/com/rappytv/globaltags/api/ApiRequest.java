a35537dcc3e86b033790715a16adf25e78d66ba8
RappyTV <rappytv@rappytv.com>
2024-04-26 15:22:58 +0200

    Modify ApiRequest class and use labymod's request classes

--- core/src/main/java/com/rappytv/globaltags/api/ApiRequest.java
+++ core/src/main/java/com/rappytv/globaltags/api/ApiRequest.java
@@ -1,15 +1,22 @@
 package com.rappytv.globaltags.api;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.rappytv.globaltags.GlobalTagAddon;
 import com.rappytv.globaltags.config.GlobalTagConfig;
 import net.labymod.api.util.I18n;
+import net.labymod.api.util.io.web.request.Callback;
+import net.labymod.api.util.io.web.request.Request;
+import net.labymod.api.util.io.web.request.Request.Method;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublisher;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public abstract class ApiRequest {
@@ -20,57 +27,38 @@ public abstract class ApiRequest {
     private String error;
     protected ResponseBody responseBody;
 
-    private final String method;
+    private final Method method;
     private final String path;
     private final String key;
 
-    public ApiRequest(String method, String path, String key) {
+    public ApiRequest(Method method, String path, String key) {
         this.method = method;
         this.path = path;
         this.key = key;
     }
 
-    public CompletableFuture<Void> sendAsyncRequest() {
-        CompletableFuture<Void> future = new CompletableFuture<>();
-
-        try {
-            // TODO: Use Request#ofGson
-            HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://gt.rappytv.com" + path))
-                .header("Content-Type", "application/json")
-                .header("Authorization", key != null ? key : "")
-                .header("X-Addon-Version", GlobalTagAddon.version)
-                .method(method, getBodyPublisher())
-                .build();
-
-            HttpClient client = HttpClient.newHttpClient();
-            client.sendAsync(request, BodyHandlers.ofString()).thenAccept(response -> {
-                responseBody = gson.fromJson(response.body(), ResponseBody.class);
+    public void sendAsyncRequest(Callback<JsonObject> callback) {
+        Request.ofGson(JsonObject.class)
+            .url("https://gt.rappytv.com" + path)
+            .method(method)
+            .body(getBody() != null ? getBody() : new HashMap<>())
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Authorization", key != null ? key : "")
+            .addHeader("X-Addon-Version", GlobalTagAddon.version)
+            .async()
+            .execute((response) -> {
+                responseBody = gson.fromJson(response.get(), ResponseBody.class);
 
                 if(responseBody.error != null) {
                     error = responseBody.error;
                     successful = false;
-                    future.complete(null);
                     return;
                 }
 
                 if(responseBody.message != null) this.message = responseBody.message;
                 successful = true;
-                future.complete(null);
-            }).exceptionally((e) -> handleException(e, future));
-        } catch (Exception e) {
-            handleException(e, future);
-        }
-
-        return future;
-    }
-
-    private Void handleException(Throwable e, CompletableFuture<Void> future) {
-        e.printStackTrace();
-        future.completeExceptionally(e);
-        successful = false;
-        error = GlobalTagConfig.exceptions ? e.getMessage() : I18n.translate("globaltags.notifications.unknownError");
-        return null;
+                callback.accept(response);
+            });
     }
 
     public boolean isSuccessful() {
@@ -82,9 +70,5 @@ public abstract class ApiRequest {
     public String getError() {
         return error;
     }
-    private BodyPublisher getBodyPublisher() {
-        if(getBody() == null) return BodyPublishers.noBody();
-        return BodyPublishers.ofString(gson.toJson(getBody()));
-    }
-    public abstract RequestBody getBody();
+    public abstract Map<String, String> getBody();
 }
