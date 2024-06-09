c942cc66b6e9f1b20e7a71027c8b2b616606ecec
RappyTV <rappytv@rappytv.com>
2024-06-09 14:16:32 +0200

    Make ToggleBanBulletPoint and ToggleAdminBulletPoint respect the player's current ban/admin state

--- core/src/main/java/com/rappytv/globaltags/interaction/ToggleBanBulletPoint.java
+++ core/src/main/java/com/rappytv/globaltags/interaction/ToggleBanBulletPoint.java
@@ -1,6 +1,7 @@
 package com.rappytv.globaltags.interaction;
 
 import com.rappytv.globaltags.GlobalTagAddon;
+import com.rappytv.globaltags.activities.BanActivity;
 import com.rappytv.globaltags.api.ApiHandler;
 import com.rappytv.globaltags.types.PlayerInfo;
 import com.rappytv.globaltags.util.TagCache;
@@ -10,13 +11,13 @@ import net.labymod.api.client.entity.player.Player;
 import net.labymod.api.client.entity.player.interaction.BulletPoint;
 import net.labymod.api.client.gui.icon.Icon;
 
-public class ToggleAdminBulletPoint implements BulletPoint {
+public class ToggleBanBulletPoint implements BulletPoint {
 
     private PlayerInfo target;
 
     @Override
     public Component getTitle() {
-        return Component.translatable("globaltags.context.admin." + (target.isAdmin() ? "remove" : "set"));
+        return Component.translatable("globaltags.context." + (target.isBanned() ? "unban" : "ban") + ".name");
     }
 
     @Override
@@ -26,11 +27,20 @@ public class ToggleAdminBulletPoint implements BulletPoint {
 
     @Override
     public void execute(Player player) {
-        ApiHandler.toggleAdmin(player.getUniqueId(), (response) -> Laby.references().chatExecutor().displayClientMessage(
-            Component
-                .text(GlobalTagAddon.prefix)
-                .append(response.getMessage())
-        ));
+        if(target.isBanned()) {
+            ApiHandler.unbanPlayer(target.getUUID(), (response) -> Laby.references().chatExecutor().displayClientMessage(
+                Component
+                    .text(GlobalTagAddon.prefix)
+                    .append(response.getMessage())
+            ));
+        } else {
+            Laby.labyAPI().minecraft().executeNextTick(() ->
+                Laby.labyAPI().minecraft().minecraftWindow().displayScreen(new BanActivity(
+                    player.getUniqueId(),
+                    player.getName()
+                ))
+            );
+        }
     }
 
     @Override
