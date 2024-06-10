e141947b5d760eda10c261bb6f69f931e33501da
RappyTV <rappytv@rappytv.com>
2024-06-10 20:50:50 +0200

    Add tag preview prototype

--- core/src/main/java/com/rappytv/globaltags/config/widget/TagPreviewWidget.java
+++ core/src/main/java/com/rappytv/globaltags/config/widget/TagPreviewWidget.java
@@ -1,10 +1,16 @@
 package com.rappytv.globaltags.config.widget;
 
+import com.rappytv.globaltags.util.TagCache;
+import net.labymod.api.Laby;
+import net.labymod.api.client.component.Component;
+import net.labymod.api.client.gui.icon.Icon;
 import net.labymod.api.client.gui.lss.property.annotation.AutoWidget;
 import net.labymod.api.client.gui.screen.Parent;
 import net.labymod.api.client.gui.screen.activity.Link;
 import net.labymod.api.client.gui.screen.widget.widgets.ComponentWidget;
-import net.labymod.api.client.gui.screen.widget.widgets.layout.FlexibleContentWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.layout.list.HorizontalListWidget;
+import net.labymod.api.client.gui.screen.widget.widgets.renderer.IconWidget;
+import net.labymod.api.client.resources.ResourceLocation;
 import net.labymod.api.configuration.settings.Setting;
 import net.labymod.api.configuration.settings.accessor.SettingAccessor;
 import net.labymod.api.configuration.settings.annotation.SettingElement;
@@ -19,14 +25,27 @@ import java.lang.annotation.Target;
 @Link("center.lss")
 @AutoWidget
 @SettingWidget
-public class TagPreviewWidget extends FlexibleContentWidget {
+public class TagPreviewWidget extends HorizontalListWidget {
+
+    private final Icon adminIcon = Icon.texture(ResourceLocation.create(
+        "globaltags",
+        "textures/icons/staff.png"
+    ));
 
     @Override
     public void initialize(Parent parent) {
         super.initialize(parent);
 
-        ComponentWidget tag = ComponentWidget.text("Test").addId("tag");
-        this.addContent(tag);
+        TagCache.resolve(Laby.labyAPI().getUniqueId(), (info) -> Laby.labyAPI().minecraft().executeOnRenderThread(() -> {
+            if(info == null) {
+                System.out.println("Info is null");
+                return;
+            }
+            ComponentWidget tag = ComponentWidget.component(info.getTag() != null ? info.getTag() : Component.empty()).addId("tag");
+            if(!info.getIconName().equals("NONE")) this.addEntry(new IconWidget(info.getIcon()).addId("icon"));
+            this.addEntry(tag);
+            if(info.isAdmin()) this.addEntry(new IconWidget(adminIcon).addId("staff-icon"));
+        }));
     }
 
     @SettingFactory
