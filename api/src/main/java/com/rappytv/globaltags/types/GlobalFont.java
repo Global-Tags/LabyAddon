97792389e733b4175f932850e30baaa22e0b140d
RappyTV <rappytv@rappytv.com>
2024-07-10 22:08:56 +0200

    Support vanilla theme

--- api/src/main/java/com/rappytv/globaltags/types/GlobalFont.java
+++ api/src/main/java/com/rappytv/globaltags/types/GlobalFont.java
@@ -1,28 +1,35 @@
 package com.rappytv.globaltags.types;
 
+import net.labymod.api.Laby;
 import net.labymod.api.client.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
 public enum GlobalFont {
-    DEFAULT(0),
-    UNICODE(-10),
-    ALAGARD(3),
-    DARK_SOULS(22),
-    GLACIAL_INDIFFERENCE(18),
-    SUPER_RPG(10);
+    DEFAULT(0, 0),
+    UNICODE(-1, -10),
+    ALAGARD(1, 3),
+    DARK_SOULS(1, 22),
+    GLACIAL_INDIFFERENCE(3, 18),
+    SUPER_RPG(2, 10);
 
     private static final Map<GlobalFont, ResourceLocation> locations = new HashMap<>();
 
-    private final double staffMargin;
+    private final double vanillaMargin;
+    private final double fancyMargin;
 
-    GlobalFont(double staffMargin) {
-        this.staffMargin = staffMargin;
+    GlobalFont(double vanillaMargin, double fancyMargin) {
+        this.vanillaMargin = vanillaMargin;
+        this.fancyMargin = fancyMargin;
     }
 
     public double getStaffMargin() {
-        return staffMargin;
+        return switch (Laby.labyAPI().themeService().currentTheme().getId()) {
+            case "vanilla" -> vanillaMargin;
+            case "fancy" -> fancyMargin;
+            default -> 0;
+        };
     }
 
     @Nullable
