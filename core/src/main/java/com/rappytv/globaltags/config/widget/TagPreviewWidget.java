eb2ee23cb63b2b19d326f98ffa9ab1e892d8a4f8
RappyTV <rappytv@rappytv.com>
2024-07-10 21:07:44 +0200

    Fix not being able to use negative values in GlobalFont constructor

--- core/src/main/java/com/rappytv/globaltags/config/widget/TagPreviewWidget.java
+++ core/src/main/java/com/rappytv/globaltags/config/widget/TagPreviewWidget.java
@@ -89,7 +89,7 @@ public class TagPreviewWidget extends HorizontalListWidget {
                             I18n.translate("globaltags.settings.tags.staged.description")
                         );
                     }
-                    setVariable("--staff-margin", config.font().get().getStaffMargin());
+                    setVariable("--staff-margin", 2 + config.font().get().getStaffMargin());
                     ComponentWidget tag = ComponentWidget.component(
                         config.tag().get().isBlank()
                             ? Component.translatable(
