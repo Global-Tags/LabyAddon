5e9cc7631f8cd8f461adbcdfa8a2273017d1fa59
RappyTV <rappytv@rappytv.com>
2024-07-26 00:12:10 +0200

    New API implementation

---
+++ api/src/main/java/com/rappytv/globaltags/api/Util.java
@@ -0,0 +1,76 @@
+package com.rappytv.globaltags.api;
+
+import com.rappytv.globaltags.wrapper.http.ApiHandler.ApiResponse;
+import net.labymod.api.Laby;
+import net.labymod.api.client.component.Component;
+import net.labymod.api.client.component.format.NamedTextColor;
+import net.labymod.api.client.gui.screen.widget.widgets.popup.SimpleAdvancedPopup;
+import net.labymod.api.notification.Notification;
+import net.labymod.api.notification.Notification.Type;
+
+public class Util {
+
+    public static final Component unchanged = Component.translatable(
+        "globaltags.settings.tags.updateSettings.unchanged",
+        NamedTextColor.DARK_GRAY
+    );
+    private static Component tagResponse = null;
+    private static Component positionResponse = null;
+    private static Component iconResponse = null;
+
+    public static void update(GlobalTagAPI api, ResultType type, Component component) {
+        switch (type) {
+            case TAG -> tagResponse = component;
+            case POSITION -> positionResponse = component;
+            case ICON -> iconResponse = component;
+        }
+        if(tagResponse == null || positionResponse == null || iconResponse == null) return;
+        SimpleAdvancedPopup popup = SimpleAdvancedPopup
+            .builder()
+            .title(Component.text("Update result", NamedTextColor.AQUA))
+            .description(Component.translatable(
+                "globaltags.settings.tags.updateSettings.result",
+                tagResponse,
+                positionResponse,
+                iconResponse
+            ))
+            .build();
+
+        Laby.labyAPI().minecraft().executeOnRenderThread(() -> {
+            popup.displayInOverlay();
+            api.getCache().clear();
+            api.getCache().resolveSelf();
+            tagResponse = null;
+            positionResponse = null;
+            iconResponse = null;
+        });
+    }
+
+    public enum ResultType {
+        TAG,
+        POSITION,
+        ICON
+    }
+
+    public static void notify(String title, String text) {
+        notify(
+            Component.text(title),
+            Component.text(text)
+        );
+    }
+
+    public static void notify(Component title, Component description) {
+        Notification.builder()
+            .title(title)
+            .text(description)
+            .type(Type.SYSTEM)
+            .buildAndPush();
+    }
+
+    public static Component getResponseComponent(ApiResponse<String> response) {
+        return Component.text(
+            response.data(),
+            response.successful() ? NamedTextColor.GREEN : NamedTextColor.RED
+        );
+    }
+}
