22d81580fed1d2bdef6cb83d7c708b08d2b0ad87
RappyTV <rappytv@rappytv.com>
2025-02-23 15:18:35 +0100

    Bump GlobalTagsJava

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,7 +3,7 @@ import net.labymod.labygradle.common.extension.LabyModAnnotationProcessorExtensi
 dependencies {
     labyProcessor()
     api(project(":api"))
-    addonMavenDependency("com.rappytv.globaltags:GlobalTagsJava:1.2.4")
+    addonMavenDependency("com.rappytv.globaltags:GlobalTagsJava:1.2.5")
 }
 
 labyModAnnotationProcessor {
