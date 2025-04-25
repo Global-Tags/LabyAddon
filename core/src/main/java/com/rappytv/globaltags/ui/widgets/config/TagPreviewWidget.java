310989dd4fc96dd6042cdb5259a66772fb907785
RappyTV <rappytv@rappytv.com>
2025-04-26 00:59:36 +0200

    Use PlayerInfo#hasCustomGlobalIcon

--- core/src/main/java/com/rappytv/globaltags/ui/widgets/config/TagPreviewWidget.java
+++ core/src/main/java/com/rappytv/globaltags/ui/widgets/config/TagPreviewWidget.java
@@ -72,7 +72,7 @@ public class TagPreviewWidget extends HorizontalListWidget {
     @Nullable
     public static String getIconUrl(@Nullable PlayerInfo<?> info, GlobalIcon icon) {
         GlobalTagAPI api = GlobalTagsAddon.getAPI();
-        if (info != null && icon == GlobalIcon.CUSTOM && info.getGlobalIconHash() != null) {
+        if (info != null && info.hasCustomGlobalIcon()) {
             return api.getUrls().getCustomIcon(api.getClientUUID(), info.getGlobalIconHash());
         }
         if (icon != GlobalIcon.NONE && icon != GlobalIcon.CUSTOM) {
