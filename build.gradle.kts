36018ddc1558b553db8336652ee8d0f014d78ecd
RappyTV <rappytv@rappytv.com>
2025-11-08 01:06:41 +0100

    chore: Use snapshot release channel, register new versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import net.labymod.labygradle.common.extension.model.labymod.ReleaseChannels
+
 plugins {
     id("net.labymod.labygradle")
     id("net.labymod.labygradle.addon")
@@ -16,6 +18,8 @@ labyMod {
         description = "Get yourself a custom GlobalTag that's publicly visible to anyone using this addon."
         minecraftVersion = "*"
         version = rootProject.version.toString()
+
+        releaseChannel.set(ReleaseChannels.SNAPSHOT)
     }
 
     minecraft {
